52b6ea37bcdde4d774d25157e7b52fe55e0b6f2f
Tomilade18 <jboyyumzey@gmail.com>
2023-07-17 13:30:57 +0100

    call

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
+function call() {
+    console.log(42)
 }
 
-countdown()
-countdown()
+call()
