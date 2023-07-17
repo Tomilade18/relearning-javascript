21634ce2471fb3a417da8c09f85fe854ab64f9e4
Tomilade18 <jboyyumzey@gmail.com>
2023-07-17 13:28:49 +0100

    count down

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// document.getElementId("count").innerText = 5
-// let count = 5
-// console.log(count)
-
-
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked 
-// change the count-el in the Html
-
-function increment() {
-     console.log("The button was clicked")
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
 }
+
+countdown()
+countdown()
