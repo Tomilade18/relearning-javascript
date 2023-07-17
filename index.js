b737a449556347df2532fd659d61518c7188ba27
Tomilade18 <jboyyumzey@gmail.com>
2023-07-17 11:27:50 +0100

    bonus points

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // document.getElementId("count").innerText = 5
 let count = 5
 
-// count + 1
-count = count + 1
-
-count = count + 10
-
-count = count - 2
-
 console.log(count)
+
+//bonus point 
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
